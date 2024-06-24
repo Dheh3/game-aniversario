3624b73a5b843247405ac5ccf8d275d5cc39bf9a
Dheh3 <dhehe361@gmail.com>
2024-06-24 19:56:13 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'engine',
+  webDir: 'www'
+};
+
+export default config;
