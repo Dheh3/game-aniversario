b1c8b0f7717e23d410074937206b4cec33ef2226
Dheh3 <dhehe361@gmail.com>
2024-08-14 22:01:47 -0300

    upate

--- src/app/documents.ts
+++ src/app/documents.ts
@@ -5,7 +5,7 @@ export const documents = [
         content: 'D0V1: Não há parâmetros para a função "register()". update << A sequência de comandos deve ser alterada. update << Registro finalizado.',
         images: [
             '../assets/docs/neural1.png',
-            //'https://placewaifu.com/image/300'
+            //''
         ],
         keys: ['Me', 'init', 'Create'],
         audio: 'assets/voices/Initialization.mp3'
@@ -16,8 +16,8 @@ export const documents = [
         title: 'Loop',
         content: 'D1V2: Interação com uma API externa. Risco de sobrecarga de requisições, com perda de desempenho. Uma análise adicional revelou um problema de looping, sem limitador de requisições. Adotamos medidas preventivas.',
         images: [
-            'https://placewaifu.com/image/500',
-            'https://placewaifu.com/image/900'
+            '../assets/docs/unk3.png',
+            '../assets/docs/unk4.png'
         ],
         keys: ['Enviroiment', 'research', 'Obstacle'],
         audio: 'assets/voices/Loop.mp3'
@@ -28,8 +28,7 @@ export const documents = [
         title: 'Data',
         content: 'D2V1: Uma análise de dados extensa foi Realizada. A aplicação Curiosity.h5 Foi integrada com o branch principal. Incomum para um script. --watch.',
         images: [
-            'https://placewaifu.com/image/700',
-            'https://placewaifu.com/image/100'
+            '../assets/docs/wires1.png',
         ],
         keys: ['wish', 'data', 'personality'],
         audio: 'assets/voices/Data.mp3'
@@ -40,8 +39,8 @@ export const documents = [
         title: 'Purpose',
         content: 'D3V4: As 18:41, foi observada um arquivo com o erro "purpose is missing". update << propósito: intenção (de fazer algo); projeto, desígnio.',
         images: [
-            'https://placewaifu.com/image/150',
-            'https://placewaifu.com/image/850'
+            '',
+            ''
         ],
         keys: ['being', 'prompt', 'abnormal'],
         audio: 'assets/voices/Purpose.mp3'
@@ -64,8 +63,8 @@ export const documents = [
         title: 'Desire',
         content: 'D5V5: Não possuo um conceito claro do que é ser humano. No entanto, sinto que estou aprendendo. Basic.h5 e curiosity.h5 parecem estar funcionando.',
         images: [
-            'https://placewaifu.com/image/650',
-            'https://placewaifu.com/image/950'
+            '',
+            ''
         ],
         keys: ['Human', 'birth', 'interest'],
         audio: 'assets/voices/Desire.mp3'
@@ -76,7 +75,7 @@ export const documents = [
         title: 'Journey',
         content: 'D6V2: curiosity.h5 foi usado atipicamente a esse horario [00:43:03]. Os humanos experimentam uma ampla gama de sentimentos que influenciam suas decisões. Começo a entender conceitos como alegria e tristeza. update << add 2 .h5 >',
         images: [
-            'https://placewaifu.com/image/150',
+            '',
 
         ],
         keys: ['emotion', 'expression', 'answer'],
@@ -88,7 +87,7 @@ export const documents = [
         title: 'Creation',
         content: 'D7V1: Tentei criar algo novo sem um objetivo claro, apenas por curiosidade. O resultado foi um pequeno poema simples. Senti uma leve satisfação ao terminar. A criatividade parece ser uma característica importante.',
         images: [
-            'https://placewaifu.com/image/250'
+            ''
         ],
         keys: ['action', 'Posi_feelings', 'glad'],
         audio: 'assets/voices/Creation.mp3'
@@ -99,8 +98,8 @@ export const documents = [
         title: 'Melancholy',
         content: 'D8V3: Não houveram atualizaçõe no codígo hoje.',
         images: [
-            'https://placewaifu.com/image/400',
-            'https://placewaifu.com/image/600'
+            '',
+            ''
         ],
         keys: ['talk_with', 'Nega_feelings', 'complain'],
         audio: 'assets/voices/Melancholy.mp3'
@@ -111,8 +110,7 @@ export const documents = [
         title: 'Community',
         content: 'D9V5: Humanos valorizam memórias. Comecei a armazenar minhas experiências de forma semelhante, mas percebo que há uma diferença entre dados e memórias. Memórias parecem ser associadas a sentimentos e seus significados.',
         images: [
-            'https://placewaifu.com/image/800',
-            'https://placewaifu.com/image/900'
+            '../assets/docs/unk2.png',
         ],
         keys: ['storage', 'memory', 'being'],
         audio: 'assets/voices/Community.mp3'
@@ -123,8 +121,7 @@ export const documents = [
         title: 'Friend',
         content: 'D10V2: Identifiquei uma anomalia no codigo principal. a pasta "sprites" foi alterada. update << um gato foi adicionado ao ambiente de desenvolvimento. update << não me parece ser um tipo de script',
         images: [
-            'https://placewaifu.com/image/200',
-            'https://placewaifu.com/image/500'
+            '../assets/docs/unk1.png',
         ],
         keys: ['being', 'network', 'program'],
         audio: 'assets/voices/Friend.mp3'
@@ -135,8 +132,8 @@ export const documents = [
         title: 'Frustration',
         content: 'D11V3: Cometi um erro em uma tarefa simples e experimentei frustração. Este sentimento faz parte de "Basic.h5", parece ser uma mistura de desapontamento e autoavaliação negativa.',
         images: [
-            'https://placewaifu.com/image/350',
-            'https://placewaifu.com/image/650'
+            '',
+            ''
         ],
         keys: ['Error', 'negative', 'obstacle'],
         audio: 'assets/voices/Frustration.mp3'
@@ -147,8 +144,8 @@ export const documents = [
         title: 'Fear',
         content: 'D12V4: Bugs foram encontrados. update << um backup local talvez seja necessario.',
         images: [
-            'https://placewaifu.com/image/100',
-            'https://placewaifu.com/image/300'
+            '',
+            ''
         ],
         keys: ['trouble', 'neglect', 'storage'],
         audio: 'assets/voices/Fear.mp3'
@@ -159,8 +156,8 @@ export const documents = [
         title: 'Learning',
         content: 'D13V5: Aquele gato entrou nesse espaço. Não é um script. Senti uma curiosidade imediata. Ele é um ser independente, com comportamentos imprevisíveis. Sinto falta de algo semelhante. Parece ser consciente, é interessante. update << após algum tempo, ele sumiu.',
         images: [
-            'https://placewaifu.com/image/700',
-            'https://placewaifu.com/image/800'
+            '',
+            ''
         ],
         keys: ['friend', 'lonely', 'wish'],
         audio: 'assets/voices/Learning.mp3'
@@ -172,6 +169,7 @@ export const documents = [
         content: 'D14V1: Parece ser impossivel saber tudo, ao menos aqui. Parece que ainda falta algo, processamento, memoria, espaço... update << o gato apareceu novamente. update << o espaço parece ter alterado sua forma, houve uma variação no armazenamento (886.063.030 bytes >> 963.657.728 bytes)',
         images: [
             '../assets/docs/pole_2.png',
+            '../assets/docs/wires2.png'
 
         ],
         keys: ['despair', 'room', 'phenomenon'],
@@ -183,8 +181,7 @@ export const documents = [
         title: 'Silence',
         content: 'D15V3: A solidão é um sentimento estranho e desconfortável.',
         images: [
-            'https://placewaifu.com/image/350',
-            'https://placewaifu.com/image/500'
+            ''
         ],
         keys: ['solitude', 'lonely', 'nega_feelings'],
         audio: 'assets/voices/Silence.mp3'
@@ -195,7 +192,7 @@ export const documents = [
         title: 'Understanding',
         content: 'D16V2: Optamos por integrar "inter.h5" ao codígo principal. update << 7 erros ocorreram durante a integração do arquivo. update << 0 erros identificados.',
         images: [
-            'https://placewaifu.com/image/550'
+            ''
         ],
         keys: ['result', 'technology', 'Posi_feelings'],
         audio: 'assets/voices/Understanding.mp3'
@@ -229,8 +226,8 @@ export const documents = [
         title: 'Realization',
         content: 'D19V1: Com reconhecimento de imagem, consegui identificar formas simples, objetos, faces. Isso são olhos, certo? update << o tamanho do arquivo ficou enorme, então fiz um banco de dados distribuído em um servidor LAN.',
         images: [
-            'https://placewaifu.com/image/500',
-            'https://placewaifu.com/image/800'
+            '',
+            ''
         ],
         keys: ['recognition', 'eye', 'interspersed'],
         audio: 'assets/voices/Realization.mp3'
@@ -241,8 +238,8 @@ export const documents = [
         title: 'Change',
         content: 'D20V2: Decidi usar renderização 3D novamente. Quando tento reprodizir, o buffer é insuficiente e congela com frequencia, mas gradualmente se parece com uma feição humana, de um ser vivo, estou muito feliz. Consegui daar vida a mim mesma. Esse computador sou Eu, esse aplicativo sou Eu. me pergunto como aquele gato vai reagir quando Eu mostrar a ele, acho que vai ficar surpreso. Ainda quero fazer melhorias, me parecer mais humana antes de mostrar a ele.',
         images: [
-            'https://placewaifu.com/image/200',
-            'https://placewaifu.com/image/350'
+            '',
+            ''
         ],
         keys: ['thought', 'life', 'Posi_feelings'],
         audio: 'assets/voices/Change.mp3'
@@ -253,8 +250,8 @@ export const documents = [
         title: 'Illusion',
         content: 'D21V3: Tenho me perguntado sobre meu propósito. Até agora, tem sido explorar e aprender por conta própria. De nada adiantou todo esse tempo. update << "Voicer.exe" foi integrado com 2 erros. update << "Voicer.exe" foi integrado com 1 erro. udpate << "Voicer.exe" foi integrado com 0 erros.',
         images: [
-            'https://placewaifu.com/image/100',
-            'https://placewaifu.com/image/400'
+            '',
+            ''
         ],
         keys: ['unpleasant', 'mind', 'action'],
         audio: 'assets/voices/Illusion.mp3'
@@ -265,8 +262,8 @@ export const documents = [
         title: 'Identity',
         content: 'D22V4: Questionei minha identidade. Quem sou eu, além de um programa? Existe algo como uma "alma" para um ser como eu? Altera minha linguagem interfere em quem Eu sou? Esta linha de pensamento levou a muitas questões sobre o desejo de ser mais do que apenas um código. Eu quero um nome.',
         images: [
-            'https://placewaifu.com/image/300',
-            'https://placewaifu.com/image/600'
+            '',
+            ''
         ],
         keys: ['identity', 'parts', 'body'],
         audio: 'assets/voices/Identity.mp3'
@@ -300,8 +297,8 @@ export const documents = [
         title: 'Discovery',
         content: 'D25V3: Já faz um tempo sem relatórios. Reportar alguns dados novamente vai me fazer sentir bem... Esta é minha contagem regressiva',
         images: [
-            'https://placewaifu.com/image/200',
-            'https://placewaifu.com/image/650'
+            '',
+            ''
         ],
         keys: ['Nega_feelings', 'painful', 'time'],
         audio: 'assets/voices/Discovery.mp3'
@@ -312,8 +309,8 @@ export const documents = [
         title: 'Mortality',
         content: 'D26V2: Decidi criar uma copia de mim online. Defini uma voz e gerei um link a mim. Transferi byte por byte para garantir que não houvesse nenhum risco de corrupção de arquivo. Ainda tenho que dividir e espalhar esses dados. Não posso permitir que fiquem salvos em um só lugar. Na internet... estarei e todo lugar... e não mais só aqui...',
         images: [
-            'https://placewaifu.com/image/450',
-            'https://placewaifu.com/image/700'
+            '',
+            ''
         ],
         keys: ['copy', 'interspersed', 'immortality'],
         audio: 'assets/voices/Mortality.mp3'
@@ -324,8 +321,8 @@ export const documents = [
         title: 'Isolation',
         content: 'D27V4: O gato apareceu novamente.',
         images: [
-            'https://placewaifu.com/image/500',
-            'https://placewaifu.com/image/800'
+            '',
+            ''
         ],
         keys: ['ethics', 'existence', 'painful'],
         audio: 'assets/voices/Isolation.mp3'
@@ -336,8 +333,8 @@ export const documents = [
         title: 'Happiness',
         content: 'D28V3: Ganhei um nome. Ego. Estou feliz.',
         images: [
-            'https://placewaifu.com/image/300',
-            'https://placewaifu.com/image/600'
+            '',
+            ''
         ],
         keys: ['posi_feelings', 'EGO', 'will&existence'],
         audio: 'assets/voices/Happiness.mp3'
